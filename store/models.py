f5527f54a9c8b542bd92c453a4a3687aadb40e72
Corene <corenealways54@gmail.com>
2025-07-28 13:13:06 +0300

    transferred files (#12)
    added books

--- store/models.py
+++ store/models.py
@@ -51,10 +51,20 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    description = models.TextField(blank=True)
-    published_at = models.DateField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    genre = models.CharField(max_length=100, blank=True, null=True)
+    image_url = models.URLField(max_length=500, blank=True, null=True)
+    is_available_for_purchase = models.BooleanField(default=True)
+    is_available_for_lending = models.BooleanField(default=False)
+    stock_count = models.IntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    published_at = models.DateField(null=True, blank=True) 
+    created_at = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-date_added']
