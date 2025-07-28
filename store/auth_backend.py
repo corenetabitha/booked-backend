3c6ea374bf397ae022cb55f5cea2c795cad0a653
Vickitah <victor.mutuma@student.moringaschool.com>
2025-07-28 09:16:22 +0300

    save completed changes

--- store/auth_backend.py
+++ store/auth_backend.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            # Treat username as email
+            
             user = User.objects.get(email=username)
         except User.DoesNotExist:
             return None
