c7658b4723c152945a65b12205b2cc7d22c99795
christine-muigai <git@github.com>
2025-07-31 09:08:17 +0300

    Auto commit

--- lendings_api/serializers.py
+++ lendings_api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from django.utils import timezone
 from .models import Lending
 from books_api.models import Book
 from users_api.serializers import UserProfileSerializer
+
 
 class LendingSerializer(serializers.ModelSerializer):
     book = serializers.StringRelatedField(read_only=True)
@@ -14,25 +16,42 @@ class LendingSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'user', 'book', 'lending_date']
 
     def create(self, validated_data):
+        request_user = self.context['request'].user
         book = validated_data.get('book_id')
         due_date = validated_data.get('due_date')
 
-        if not book:
-            raise serializers.ValidationError({"book_id": "This field is required."})
+        
+        if Lending.objects.filter(user=request_user, book=book, status__in=['pending', 'borrowed']).exists():
+            raise serializers.ValidationError(
+                "You already have an active lending request or borrowed this book."
+            )
 
         lending = Lending.objects.create(
-            user=self.context['request'].user,
+            user=request_user,
             book=book,
-            due_date=due_date
+            due_date=due_date,
+            status='pending'  
         )
         return lending
+
 
 class ReturnRequestSerializer(serializers.Serializer):
     lending_id = serializers.IntegerField()
     returned = serializers.BooleanField()
 
     def validate_lending_id(self, value):
-        if not Lending.objects.filter(id=value).exists():
+        try:
+            lending = Lending.objects.get(id=value)
+        except Lending.DoesNotExist:
             raise serializers.ValidationError("Lending with this ID does not exist.")
         return value
+
+    def update(self, instance, validated_data):
+        if validated_data['returned']:
+            instance.returned_date = timezone.now()
+            instance.status = 'returned'
+        else:
+            instance.returned_date = None  # Optional: clear if un-returning
+        instance.save()
+        return instance
 
